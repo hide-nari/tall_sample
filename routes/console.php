9922966f509efdd9022b33b938638fceb2ec74de
hide-nari <hide3@mild.ocn.ne.jp>
2026-09-20 08:01:21 +0900

    Add seeder command

--- routes/console.php
+++ routes/console.php
@@ -1,8 +1,27 @@
 <?php
 
+use App\Models\Department;
+use App\Models\Employee;
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
+
+Artisan::command('cli:department_add', function () {
+    Department::create(
+        [
+            'name' => 'CLI make dept:'.now(),
+        ]
+    );
+})->purpose('Add department sample record');
+
+Artisan::command('cli:employee_add', function () {
+    Employee::create(
+        [
+            'name' => 'CLI make name:'.now(),
+            'email' => 'test'.random_int(10, 99).'@test.com',
+        ]
+    );
+})->purpose('Add employee sample record');
